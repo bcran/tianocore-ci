16c12b68c871c0760a21a4400ac0aea7e841c493
Rebecca Cran <rebecca@bsdio.com>
2021-06-12 18:05:44 -0600

    .

--- setup.cmd
+++ setup.cmd
@@ -1,18 +1,21 @@
 set EDK2REPO=https://github.com/tianocore/edk2.git
 
 if not exist edk2 (
-	git clone %EDK2REPO% 
+	git clone %EDK2REPO% || exit /b
 ) else (
 	cd edk2
 	git fsck
 	if "%ERRORLEVEL%" NEQ "0" (
 		cd ..
-		git clone %EDK2REPO%
+		git clone %EDK2REPO% || exit /b
 	) else (
-		git clean -fdx
+		git clean -fdx || exit /b
+		cd ..
 	)
 )
 
 cd edk2
-git submodule update --init --force
-edksetup.bat Reconfig ForceRebuild VS2019
+git submodule update --init --force || exit /b
+edksetup.bat ForceRebuild VS2019 || exit /b
+edksetup.bat Reconfig || exit /b
+
