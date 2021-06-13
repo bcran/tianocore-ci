cb1611a1e91340aa15efc7c7386cb959d6e62e2f
Rebecca Cran <rebecca@bsdio.com>
2021-06-12 20:24:21 -0600

    .

--- build_ovmf.cmd
+++ build_ovmf.cmd
@@ -1,6 +1,6 @@
 cd edk2
 set WORKSPACE=%CD%
-edksetup.bat reconfig || exit /b
+edksetup.bat reconfig vs2019
 
 build -p OvmfPkg/OvmfPkgX64.dsc -t VS2019 -a X64 -b RELEASE || exit /b
 build -p OvmfPkg/OvmfPkgX64.dsc -t VS2019 -a X64 -b DEBUG || exit /b
