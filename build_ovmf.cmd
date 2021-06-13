fcbed24ed9370686bbb05dd54a881c89b6c338ff
Rebecca Cran <rebecca@bsdio.com>
2021-06-12 22:37:15 -0600

    .

--- build_ovmf.cmd
+++ build_ovmf.cmd
@@ -1,11 +1,6 @@
 cd edk2
 set WORKSPACE=%CD%
-echo Running edksetup.bat
-edksetup.bat reconfig vs2019
-echo "Environment configured. Building..."
-set
-echo %WORKSPACE%
-where build
+call edksetup.bat reconfig vs2019
 
 build -p OvmfPkg/OvmfPkgX64.dsc -t VS2019 -a X64 -b RELEASE || exit /b
 build -p OvmfPkg/OvmfPkgX64.dsc -t VS2019 -a X64 -b DEBUG || exit /b
