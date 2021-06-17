62f9ab13e4f0dba36b7a82e925f7ebc9bfaee590
Rebecca Cran <rebecca@bsdio.com>
2021-06-16 20:18:24 -0600

    Add macOS x64 OVMF build file

---
+++ build/build_ovmf.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+while getopts "t:a:b:p:" OPTION; do
+	case $OPTION in
+	t)
+		TOOLCHAIN=$OPTARG
+		;;
+	a)
+		ARCH="$ARCH $OPTARG"
+		;;
+	b)
+		BUILD=$OPTARG
+		;;
+	p)
+		PLATFORM=$OPTARG
+		;;
+	*)
+		echo "Invalid option"
+		exit 1
+		;;
+	esac
+done
+
+cd edk2
+WORKSPACE=$PWD
+TOOLCHAIN=${TOOLCHAIN:-GCC5}
+
+. ./edksetup.sh
+
+build -p $PLATFORM -t $TOOLCHAIN -a $ARCH -b $BUILD
