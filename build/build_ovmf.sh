bc58bcc336adb1cd7bd9c7905b985f90ac29b139
Rebecca Cran <rebecca@bsdio.com>
2021-06-16 21:52:54 -0600

    .

--- build/build_ovmf.sh
+++ build/build_ovmf.sh
@@ -1,36 +1,17 @@
 #!/bin/bash
 
 set -e
-set -x
-
-while getopts "t:a:b:p:" OPTION; do
-	case $OPTION in
-	t)
-		T=$OPTARG
-		;;
-	a)
-		A="$ARCH $OPTARG"
-		;;
-	b)
-		B=$OPTARG
-		;;
-	p)
-		P=$OPTARG
-		;;
-	*)
-		echo "Invalid option"
-		exit 1
-		;;
-	esac
-done
 
 cd ../edk2
 #WORKSPACE=$PWD
-T=${T:-GCC5}
-
 
 echo "PWD is $PWD"
 #echo "WORKSPACE is $WORKSPACE"
 . ./edksetup.sh
 
-build -p $P -t $T -a $A -b $B
+ARCH2=
+if [ -v A2 ]; then
+	ARCH2="-a $A2"
+fi
+
+build -p $P -t $T -a $A $ARCH2 -b $B
